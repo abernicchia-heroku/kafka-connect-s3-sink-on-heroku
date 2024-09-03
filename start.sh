ab14ddca6340c86ab227b1cbf723aee15cbd1f60
Andrea Bernicchia <abernicchia@salesforce.com>
2024-09-03 13:26:52 +0200

    comments added

--- start.sh
+++ start.sh
@@ -20,6 +20,8 @@ export CONNECT_BOOTSTRAP_SERVERS="${!kafka_url_env_var}"
 # export REST and ADVERTISED hostname/port required by Kafka Connect
 # https://docs.confluent.io/platform/current/installation/docker/config-reference.html#required-kconnect-long-configurations
 # https://devcenter.heroku.com/articles/dyno-dns-service-discovery#environment-variables
+# HEROKU_PRIVATE_IP is preferred over HEROKU_DNS_DYNO_NAME due to DNS registry update frequency (10s) https://devcenter.heroku.com/articles/dyno-dns-service-discovery#dns-registry-details
+# HEROKU_PRIVATE_IP is emitted at dyno start (see below) to match IP with dyno name (e.g. web.2)
 export CONNECT_REST_ADVERTISED_HOST_NAME="${HEROKU_PRIVATE_IP}"
 export CONNECT_REST_ADVERTISED_PORT=2020
 export CONNECT_LISTENERS="HTTP://0.0.0.0:${PORT},HTTP://0.0.0.0:2020"
